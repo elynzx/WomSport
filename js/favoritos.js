3fe7d6ff104b16d5d46c34e3ddb3da62ffd4a6eb
PaolaArtola <marifeartola@gmail.com>
2025-06-09 11:52:41 -0500

    add menu

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,4 +1,20 @@
-window.onload = function(){
-    const amountFav = document.getElementById("cant_fav");
-    amountFav.textContent = "Todos los artículos (14)"; 
+window.onload = function () {
+  const amountFav = document.getElementById("cant_fav");
+  amountFav.textContent = "Todos los artículos (14)";
+};
+
+function menuHandler() {
+  return {
+    open: false,
+    currentId: null,
+    menuX: 0,
+    menuY: 0,
+    openMenu(id, event) {
+      this.currentId = id;
+      const rect = event.target.getBoundingClientRect();
+      this.menuX = rect.left + window.scrollX;
+      this.menuY = rect.bottom + window.scrollY;
+      this.open = true;
+    },
+  };
 }
