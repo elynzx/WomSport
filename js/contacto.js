d650bafef8a244172a2c43bca3b4a6fb61b30370
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-07-16 08:43:52 -0500

    correccion de emailjs

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,4 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
+/* document.getElementById("contact-form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     emailjs.sendForm("service_0b7ux3a", "template_ddmqx91", this)
@@ -10,4 +10,21 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
             console.error("Error al enviar", error);
             mostrarModal("Hubo un problema al enviar el mensaje");
         });
-});
+}); */
+
+/* document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_0b7ux3a", "template_ddmqx91", this)
+      .then(() => {
+        alert("¡Mensaje enviado correctamente! 🎉");
+        form.reset();
+      }, (error) => {
+        console.error("Error al enviar:", error);
+        alert("Hubo un problema al enviar tu mensaje. Por favor intenta de nuevo.");
+      });
+  });
+}); */
