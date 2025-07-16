f1b6c3d8826f6d6785e077bc164de86d0b99a1c6
Paola Artola <130616755+PaolaArtola@users.noreply.github.com>
2025-07-16 08:52:38 -0500

    add btn pagar

--- js/carrito.js
+++ js/carrito.js
@@ -5,12 +5,11 @@ const globa_carrito = new Carrito();
 
 function productCard() {
   return {
-    products: [
-    ],
+    products: [],
 
     init() {
-      itemsHTML.style.display = 'none';
-      loading.style.display = 'flex';
+      itemsHTML.style.display = "none";
+      loading.style.display = "flex";
       fetch("/data/productos.json")
         .then((response) => response.json())
         .then((productos) => {
@@ -20,8 +19,8 @@ function productCard() {
           console.error("Error loading products:", error);
         });
       console.log("Getting products for Alpine.js:");
-      loading.style.display = 'none';
-      itemsHTML.style.display = 'flex';
+      loading.style.display = "none";
+      itemsHTML.style.display = "flex";
     },
 
     totalQuantity(product) {
@@ -34,38 +33,44 @@ function productCard() {
 
     updateQuantity(productIndex, sizeIndex, change) {
       const product = this.products[productIndex];
-      const amount = Math.max(
-        0,
-        product.sizes[sizeIndex].quantity + change
-      );
+      const amount = Math.max(0, product.sizes[sizeIndex].quantity + change);
       product.sizes[sizeIndex].quantity = amount;
-      globa_carrito.remove_item(product.id, product.sizes[sizeIndex].alias, -change)
+      globa_carrito.remove_item(
+        product.id,
+        product.sizes[sizeIndex].alias,
+        -change
+      );
     },
 
     removeSize(productIndex, sizeIndex) {
       const product = this.products[productIndex];
-      const size = product.sizes[sizeIndex]
+      const size = product.sizes[sizeIndex];
       globa_carrito.remove_item(product.id, size.alias, size.quantity + 1);
       this.products[productIndex].sizes.splice(sizeIndex, 1);
 
       if (this.products[productIndex].sizes.length == 0) {
         this.init();
       }
-
     },
 
     deleteProduct(productIndex) {
-      const product = this.products[productIndex]
-      if (confirm('Are you sure you want to delete this product?')) {
+      const product = this.products[productIndex];
+      if (confirm("Are you sure you want to delete this product?")) {
         this.products.splice(productIndex, 1);
         globa_carrito.delete_product(product.id);
       }
     },
+
     grandTotal() {
-      return this.products.reduce((sum, product) => {
-        const quantity = product.sizes.reduce((q, size) => q + size.quantity, 0);
-        return sum + quantity * product.cost;
-      }, 0).toFixed(2);
+      return this.products
+        .reduce((sum, product) => {
+          const quantity = product.sizes.reduce(
+            (q, size) => q + size.quantity,
+            0
+          );
+          return sum + quantity * product.cost;
+        }, 0)
+        .toFixed(2);
     },
   };
 }
